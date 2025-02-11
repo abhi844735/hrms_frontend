29a10577c4551b9d257b7571768a129bbf6a1083
maniram958 <manimaniram301@gmail.com>
2025-02-11 23:11:07 +0530

    added endpoint

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import AppProvider from '../src/context/AppContext.jsx'
 import { Toaster } from 'react-hot-toast'
 
 // window.apiURL = "http://localhost:8080/api/v1"4
-window.apiURL = "https://hrms-backend-plum.vercel.app";
+window.apiURL = "https://hrms-backend-plum.vercel.app/api/v1";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
